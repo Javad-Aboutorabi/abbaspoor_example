b2ecc7d6ef9ef11650c511894b47c8bd0331571c
javad <javad.aboutorabi>
2023-01-29 16:46:09 +0330

    refactor: add function to a method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import  java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        System.out.println("result = "+ sum());
+    }
+
+    private static Integer sum() {
         Scanner scanner= new Scanner(System.in);
         System.out.println("Please input 2 Number : ");
         int input1=scanner.nextInt();
         int input2=scanner.nextInt();
-        int sum=input1+input2;
-        System.out.println("result = "+sum);
+        return input1+input2;
 
     }
 }
