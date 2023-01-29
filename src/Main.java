a538a59ea9e620ba0c01c191d9fe3176314cd4c7
abbaspour <m.abaspour74@gmail.com>
2023-01-29 16:37:30 +0330

    feat : build sum function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import  java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please input 2 Number : ");
+        int input1=scanner.nextInt();
+        int input2=scanner.nextInt();
+        int sum=input1+input2;
+        System.out.println("result = "+sum);
+
     }
 }
