f8444ee0f63116868e0ff293f7c5284adbccd31a
abbaspour <m.abaspour74@gmail.com>
2023-01-30 10:43:24 +0330

    feat :  Build Arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-import  java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("result = "+ sum());
+    public static void main(String[] args) {
+        fildlist(10);
     }
-
-    private static Integer sum() {
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Please input 2 Number : ");
-        int input1=scanner.nextInt();
-        int input2=scanner.nextInt();
-        return input1+input2;
-
+    public static int fildlist(int x){
+        Scanner input= new Scanner(System.in);
+        ArrayList list= new ArrayList();
+        Random r= new Random();
+        int i = 0;
+        while (i < x){
+            int b= r.nextInt(100);
+            list.add(b);
+            i++;
+        }
+        System.out.println(list);
+        return x;
     }
 }
