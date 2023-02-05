0c85ba8e6f713540e2b67fa2a2c235a16d36a45f
javad <javad.aboutorabi>
2023-02-05 11:55:38 +0330

    feat: find the biggest number in a list

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList finalList = filList(5);
+        ArrayList<Integer> finalList = filList(10);
+
+
         System.out.println(finalList);
+        System.out.println(findMax(finalList));
+
+
     }
 
     public static ArrayList<Integer> filList(int x) {
-        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Integer> list = new ArrayList<>();
         Random r = new Random();
         int i = 0;
         while (i < x) {
@@ -16,6 +22,32 @@ public class Main {
             list.add(b);
             i++;
         }
+
         return list;
+    }
+
+
+    public static Integer findMax(ArrayList<Integer> list) {
+        int index = 0;
+        int max = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            if (max <= list.get(i)) {
+                index = i;
+                max = list.get(i);
+            }
+        }
+
+        int temp = list.get(list.size()-1);
+        list.set(list.size()-1,list.get(index));
+        list.set(index,temp);
+
+//        System.out.println("------------------------------------------------------------");
+//        System.out.println(list);
+
+
+
+
+
+        return max;
     }
 }
