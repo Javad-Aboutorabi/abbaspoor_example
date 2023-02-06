e5ef915e2ccda5212cd748ef003a09cbe03ec026
abbaspour <m.abaspour74@gmail.com>
2023-02-06 15:10:56 +0330

    refactor :  build of average function

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(filList());
+        Average();
 
     }
 
-    public static ArrayList<Integer> filList() {
+    public static int Average() {
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
         list.add(10);
         list.add(5);
         list.add(-7);
         list.add(15);
         list.add(28);
         list.add(3);
-        System.out.println(list);
-        for (int i = list.size()-1; i>=0; i--){
-            list2.add(list.get(i));
+        int average;
+        int sum = 0;
 
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            sum = list.get(i) + sum;
         }
-        System.out.println(list2);
-        return list;
+        average = sum / list.size();
+        System.out.println("Average is : " + average);
+        return average;
     }
 
 }
